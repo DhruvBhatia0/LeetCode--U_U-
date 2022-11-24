15da4404ff7f27600918b973e00fbb5a51bbf685
DhruvBhatia0 <69252327+DhruvBhatia0@users.noreply.github.com>
2022-11-23 23:18:40 -0500

    Time: 215 ms (5.04%), Space: 14.4 MB (30.07%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        best_way = {} #maps an index, with the least cost to get to the end
+        
+        def dfs(i):
+            if (i == len(days)):
+                return 0 #no cost if there are no days to travel on
+            if (i in best_way.keys()):
+                return best_way[i]
+            
+            best_way[i] = float("inf")
+            
+            for day, cost in zip([1,7,30], costs):
+                j = i
+                while (j < len(days) and days[j] < days[i] + day):
+                    j += 1
+                    best_way[i] = min(best_way[i], cost + dfs(j))
+            return best_way[i]
+            
+        
+        return dfs(0)
