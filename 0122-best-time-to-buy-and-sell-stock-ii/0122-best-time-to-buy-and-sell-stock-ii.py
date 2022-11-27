7ee87e3a370bd28702f91b2e98e2171227f71461
DhruvBhatia0 <69252327+DhruvBhatia0@users.noreply.github.com>
2022-11-27 18:14:46 -0500

    Time: 129 ms (55.99%), Space: 15.3 MB (25.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -8,25 +8,19 @@ class Solution:
             if (buyTime):
                 if prices[i] > prices[i+1]:
                     i = i + 1
-                    continue
                 elif prices[i] < prices[i+1]:
-                    print(i)
                     buyTime = False
                     profit = profit - prices[i]
                     last_buy = prices[i]
                     i = i + 1
-                    continue
                 else:
                     i = i+1
             else:
                 if prices[i] > prices[i+1]:
-                    print(i)
                     buyTime = True
                     profit = profit + prices[i]
-                    continue
                 else:
                     i = i+1
-                    continue
         if not buyTime:
             if prices[len(prices)-1] >= prices[len(prices)-2]:
                 profit = profit + prices[len(prices)-1]
