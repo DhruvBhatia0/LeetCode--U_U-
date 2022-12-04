807a6f8117be11a91df3635f9eed94076c0065e7
DhruvBhatia0 <69252327+DhruvBhatia0@users.noreply.github.com>
2022-12-03 19:43:04 -0500

    Time: 58 ms (63.32%), Space: 13.9 MB (13.23%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.py
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.py
@@ -1,13 +1,3 @@
 class Solution:
     def minCostToMoveChips(self, position: List[int]) -> int:
-        count_odd = 0
-        count_even = 0
-        for i in range(len(position)):
-            if position[i] % 2 == 1:
-                count_odd += 1
-            else:
-                count_even += 1
-        
-        if (count_even > count_odd):
-            return count_odd
-        return count_even
+        return min((odd := sum(p%2 for p in position)), len(position) - odd)
